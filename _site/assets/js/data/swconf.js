e8da39e863e56a4b7887c5d7381139ec5d45354a
Shivendra Prajapati <lhtshivendra@gmail.com>
2025-07-02 11:47:20 +0530

    Billing Writeup

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1751284243',resources: [
+    cacheName: 'chirpy-1751436830',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
