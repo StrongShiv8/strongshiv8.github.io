3fb9ac36c6366b731981b492f53028fd7e1754f4
Shivendra Prajapati <lhtshivendra@gmail.com>
2025-07-31 17:43:20 +0530

    Takedown Walkthrough Added

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1753780279',resources: [
+    cacheName: 'chirpy-1753963903',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
