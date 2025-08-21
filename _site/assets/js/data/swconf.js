4000a59cd1f3cf248ec3c4bb5074166effadf371
Shivendra Prajapati <lhtshivendra@gmail.com>
2025-08-21 14:47:08 +0530

    ...

---
+++ _site/assets/js/data/swconf.js
@@ -0,0 +1,45 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1755767790',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/categories/',
+      
+        '/tags/',
+      
+        '/archives/',
+      
+        '/about/',
+      
+
+      
+      
+        '/assets/images/favicons/android-chrome-192x192.png',
+        '/assets/images/favicons/android-chrome-512x512.png',
+        '/assets/images/favicons/apple-touch-icon.png',
+        '/assets/images/favicons/favicon-16x16.png',
+        '/assets/images/favicons/favicon-32x32.png',
+        '/assets/images/favicons/favicon.ico',
+        '/assets/js/dist/categories.min.js',
+        '/assets/js/dist/commons.min.js',
+        '/assets/js/dist/home.min.js',
+        '/assets/js/dist/misc.min.js',
+        '/assets/js/dist/page.min.js',
+        '/assets/js/dist/post.min.js',
+        '/assets/js/dist/theme.min.js'
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+          'https://strongshiv8.goatcounter.com/counter/'
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
