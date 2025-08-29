3f463e98c2f8ce160fca228247a73f31e2e63d0a
Shivendra Prajapati <lhtshivendra@gmail.com>
2025-08-29 21:28:01 +0530

    Hoshi HackMyVM Walkthrough Added

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1756035946',resources: [
+    cacheName: 'chirpy-1756482976',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
