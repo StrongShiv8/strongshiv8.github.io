ad62c3ddc843988bb4b4431602dd3fe55933336e
Shivendra Prajapati <lhtshivendra@gmail.com>
2025-08-30 10:47:03 +0530

    ...

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1756482976',resources: [
+    cacheName: 'chirpy-1756530799',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
