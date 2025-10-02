2eae62729b6476bd6afe45c8ac47590595c0ef12
Shivendra Prajapati <lhtshivendra@gmail.com>
2025-10-02 11:14:27 +0530

    Multi HackMyVM Machine added

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1758000723',resources: [
+    cacheName: 'chirpy-1759383775',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
